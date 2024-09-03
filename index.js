a5c7d7360079fabf9e039b17add36054eef86374
Noor <premb4999@gmail.com>
2024-09-03 17:06:36 +0530

    First commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const age = 24;
 const name = 'Noor';
 
-const dob = '01/01/2024'
-const isAdult = () +> {
-    return age >= 8;
+const dob = '01/01/2024q
+const isADult = () > {
+    return age > 15;
 }
